14902bd80d8c6fa427b07e45621e92029b4a2ebc
Sebastian Koslowski <sebastian.koslowski@gmail.com>
2023-11-23 17:58:28 +0100

    switch to litestar

--- src/simple_python_package_index/main.py
+++ src/simple_python_package_index/main.py
@@ -1,21 +1,19 @@
 import logging
-from contextlib import asynccontextmanager
+from enum import StrEnum
 from pathlib import Path
+from typing import Any
 
-from fastapi import FastAPI, Request, Response
-from fastapi.responses import (
-    FileResponse,
-    HTMLResponse,
-    JSONResponse,
-    PlainTextResponse,
-    RedirectResponse,
-)
 from furl import furl
+from litestar import Controller, Litestar, MediaType, Request, Response, Router, get
+from litestar.di import Provide
+from litestar.response import File, Redirect
+from litestar.serialization import default_serializer
+from litestar.types import Serializer
 from packaging.utils import canonicalize_name
 from pydantic import DirectoryPath
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from . import __version__, html, loader, model
+from . import html, loader
 
 
 class Settings(BaseSettings):
@@ -29,14 +27,118 @@ class Settings(BaseSettings):
 settings = Settings()
 logger = logging.getLogger(__name__)
 
-index_data = loader.SimpleIndexCollection(
-    data_dir=settings.files_dir,
-    url=str(furl(settings.root_path) / settings.files_url),
-)
+
+class PyPISimpleResponse[T](Response[T]):
+    def render(self, content: Any, media_type: str, enc_hook: Serializer = default_serializer) -> bytes:
+        media_type = {"json": MediaType.JSON, "html": MediaType.HTML}.get(
+            media_type.rpartition("+")[-1], media_type
+        )
+        return super().render(content, media_type, enc_hook)
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
+class PPSMediaType(StrEnum):
+    JSON_V1 = "application/vnd.pypi.simple.v1+json"
+    HTML_V1 = "application/vnd.pypi.simple.v1+html"
+
+
+def get_response_type(request: Request) -> PPSMediaType | None:
+    html = {
+        PPSMediaType.HTML_V1,
+        "application/vnd.pypi.simple.latest+html",
+        MediaType.HTML,
+    }
+    if html.intersection(request.accept):
+        return PPSMediaType.HTML_V1
+
+    json = {
+        PPSMediaType.JSON_V1,
+        "application/vnd.pypi.simple.latest+json",
+    }
+    if json.intersection(request.accept):
+        return PPSMediaType.JSON_V1
+
+
+class SimpleIndexView(Controller):
+    path = "simple"
+
+    @get("/", sync_to_thread=False)
+    def index(self, request: Request, simple_index: loader.SimpleIndex) -> Response[loader.Index | str]:
+        match get_response_type(request):
+            case PPSMediaType.JSON_V1:
+                return PyPISimpleResponse(
+                    content=simple_index.index,
+                    media_type=PPSMediaType.JSON_V1,
+                )
+            case PPSMediaType.HTML_V1:
+                return PyPISimpleResponse(
+                    content=str(html.generate_index(simple_index.index, request.url.path)),
+                    media_type=PPSMediaType.HTML_V1,
+                )
+
+        return Response("No acceptable format found", status_code=406)
+
+    @get("{project_name:str}/", sync_to_thread=False)
+    def project_detail(
+        self, request: Request, project_name: str, simple_index: loader.SimpleIndex
+    ) -> Response[loader.Details | str]:
+        name = canonicalize_name(project_name)
+        if name != project_name:
+            return Redirect(
+                path=request.url.path.replace(project_name, name),
+                status_code=301,
+            )
+
+        try:
+            project_details = simple_index[name]
+        except KeyError:
+            return Response("Project can not be found", status_code=404)
+
+        match get_response_type(request):
+            case PPSMediaType.JSON_V1:
+                return PyPISimpleResponse(
+                    content=project_details,
+                    media_type=PPSMediaType.JSON_V1,
+                )
+            case PPSMediaType.HTML_V1:
+                return PyPISimpleResponse(
+                    content=str(html.generate_project_page(project_details)),
+                    media_type=PPSMediaType.HTML_V1,
+                )
+
+        return Response("No acceptable format found", status_code=406)
+
+
+def get_path(file: Path) -> Path | None:
+    if file.is_absolute():
+        file = file.relative_to("/")
+    files_dir = settings.files_dir.resolve()
+    file_on_disk = files_dir.joinpath(file).resolve()
+    if files_dir in file_on_disk.parents:
+        return file_on_disk
+
+
+@get(settings.files_url + "/{file:path}")
+async def files(request: Request, file: Path, index_data: loader.SimpleIndexCollection) -> Response:
+    if file.suffix == ".metadata" and (content := index_data.get_meta_data(request.url.path)):
+        return Response(content, headers={"Content-Disposition": f"attachment; filename={file.name}"})
+    elif (filepath := get_path(file)) and filepath.is_file():
+        return File(filepath)
+
+    return Response("File not found", status_code=404)
+
+
+@get("/ping")
+async def ping() -> None:
+    return  # docker health-check
+
+
+def provide(obj: Any) -> Provide:
+    provider = Provide(lambda: None, use_cache=True, sync_to_thread=True)
+    provider.value = obj
+    return provider
+
+
+def main() -> Litestar:
     handler = logging.StreamHandler()
     handler.setFormatter(logging.Formatter("%(levelname)s:     %(message)s"))
     logger.addHandler(handler)
@@ -45,131 +147,30 @@ async def lifespan(app: FastAPI):
     logger.info("Wheels files are searched in %s", settings.files_dir)
     logger.info("Root path is %s", settings.root_path)
 
+    index_data = loader.SimpleIndexCollection(
+        data_dir=settings.files_dir,
+        url=str(furl(settings.root_path) / settings.files_url),
+    )
     index_data.reload()
-    for name, index in sorted(index_data.indexes()):
-        name = f"Index '{name}'" if name else "Root index"
-        logger.info(
-            f"{name} with {len(index.projects)} projects and {len(index.metadata)} distributions"
+
+    app = Litestar(
+        route_handlers=[ping, files],
+        dependencies={"index_data": provide(index_data)},
+        debug=True,
+    )
+
+    for name, index_ in sorted(index_data.indexes()):
+        name_ = f"Index '{name}'" if name else "Root index"
+        logger.info(f"{name_} with {len(index_.projects)} projects and {len(index_.metadata)} distributions")
+
+        router = Router(
+            name,
+            route_handlers=[SimpleIndexView],
+            dependencies={"simple_index": provide(index_)},
         )
+        app.register(router)
 
-    yield
-
-app = FastAPI(
-    name=__package__,
-    version=__version__,
-    root_path=settings.root_path,
-    lifespan=lifespan,
-)
+    return app
 
 
-class PyPISimpleV1JSONResponse(JSONResponse):
-    media_type = "application/vnd.pypi.simple.v1+json"
-    acceptable_types = {media_type, "application/vnd.pypi.simple.latest+json"}
-
-
-class PyPISimpleV1HTMLResponse(HTMLResponse):
-    media_type = "application/vnd.pypi.simple.v1+html"
-    acceptable_types = {
-        media_type,
-        "application/vnd.pypi.simple.latest+html",
-        "text/html",
-        "*/*",
-    }
-
-
-class NotAcceptableResponse(HTMLResponse):
-    def __init__(self) -> None:
-        super().__init__(status_code=406)
-
-
-def content_negotiation(request: Request) -> type[Response]:
-    accept = set(request.headers.get("accept", "*/*").split(","))
-    for cls in (PyPISimpleV1JSONResponse, PyPISimpleV1HTMLResponse):
-        if cls.acceptable_types & accept:
-            return cls
-    return NotAcceptableResponse
-
-
-@app.get(
-    "/simple/",
-    response_class=PyPISimpleV1JSONResponse,
-    response_model=model.Index,
-    response_model_exclude_none=True,
-)
-@app.get(
-    "/{collection}/simple/",
-    response_class=PyPISimpleV1JSONResponse,
-    response_model=model.Index,
-    response_model_exclude_none=True,
-)
-def index(request: Request, collection: str = ""):
-    response_type = content_negotiation(request)
-    if response_type is NotAcceptableResponse:
-        return NotAcceptableResponse()
-
-    index = index_data[collection].index
-    if response_type is PyPISimpleV1HTMLResponse:
-        return PyPISimpleV1HTMLResponse(str(html.generate_index(index, request.url.path)))
-
-    return index
-
-
-@app.get(
-    "/simple/{project_name}/",
-    response_class=PyPISimpleV1JSONResponse,
-    response_model=model.Details,
-    response_model_exclude_none=True,
-)
-@app.get(
-    "/{collection}/simple/{project_name}/",
-    response_class=PyPISimpleV1JSONResponse,
-    response_model=model.Details,
-    response_model_exclude_none=True,
-)
-def project_detail(request: Request, project_name: str, collection: str = ""):
-    name = canonicalize_name(project_name)
-    if name != project_name:
-        return RedirectResponse(
-            url=request.url.path.replace(project_name, name),
-            status_code=301,
-        )
-
-    response_type = content_negotiation(request)
-    if response_type is NotAcceptableResponse:
-        return NotAcceptableResponse()
-
-    try:
-        project_details = index_data[collection][name]
-    except KeyError:
-        return HTMLResponse("Can't find this project", status_code=404)
-
-    if response_type is PyPISimpleV1HTMLResponse:
-        content = str(html.generate_project_page(project_details))
-        return PyPISimpleV1HTMLResponse(content)
-
-    return project_details
-
-
-def get_path(file: Path) -> Path | None:
-    if file.is_absolute():
-        file = file.relative_to("/")
-    file_on_disk = settings.files_dir.joinpath(file).resolve()
-    if settings.files_dir in file_on_disk.parents:
-        return file_on_disk
-
-
-@app.get(settings.files_url + "/{file:path}")
-def files(request: Request, file: Path):
-    if file.suffix == ".metadata" and (content := index_data.get_meta_data(request.url.path)):
-        return PlainTextResponse(
-            content, headers={"Content-Disposition": f"attachment; filename={file.name}"}
-        )
-    elif (filepath := get_path(file)) and filepath.is_file():
-        return FileResponse(filepath)
-
-    return HTMLResponse(status_code=404)
-
-
-@app.get("/ping")
-async def ping():
-    return {}  # docker health-check
+app = main()
